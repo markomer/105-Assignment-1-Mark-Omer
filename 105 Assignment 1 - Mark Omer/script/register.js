fbe6cc1279bc7f7b660de8bd472a503664426620
markomer <97204116+markomer@users.noreply.github.com>
2022-02-19 09:05:33 -0700

    Add files via upload

---
+++ 105 Assignment 1 - Mark Omer/script/register.js
@@ -0,0 +1,99 @@
+//create the constructor ES2015 / class
+class User{
+  //this are the parameters <----------------------------->
+  constructor(email,password,first,last,age,address,phone,payment,color){
+    this.email=email;
+    this.password=password;
+    this.fname=first; // **
+    this.lname=last; // **
+    this.age=age;
+    this.address=address;
+    this.phone=phone;
+    this.payment=payment;
+    this.color=color;
+  }
+}
+
+// this is a local var-->
+function isValid(user){
+  //return false when the user is not valid
+  //return true when the user is valid
+  let valid=true;
+  //we need to reset the origianl appearance of inputs
+  //by removing the class alert-error
+  $("input").removeClass("alert-error");
+  //validations
+  if(user.email.length==0){
+    // if we get here it means that the eamil is empty
+    valid=false;
+    $("#txtEmail").addClass("alert-error");
+    console.log("Missing email");
+  }
+  if(user.password.length==0){
+    valid=false;
+    $("#txtPassword").addClass("alert-error");
+    console.log("Missing Password");
+  }
+
+  if(user.fname.length==0){
+    valid=false;
+    $("#txtFirst").addClass("alert-error");
+    console.log("Missing First Name");
+  }
+
+  if(user.lname.length==0){
+    valid=false;
+    $("#txtLast").addClass("alert-error");
+    console.log("Missing Last Name");
+  }
+
+  if(user.age.length==0){
+    valid=false;
+    $("#txtAge").addClass("alert-error");
+    console.log("Missing Age");
+  }
+
+  return valid;
+}
+
+
+function registerUser(){
+  let email = $("#txtEmail").val(); // *check your id on the html
+  let password = $("#txtPassword").val();
+  let first = $("#txtFirst").val();
+  let last = $("#txtLast").val();
+  let age = $("#txtAge").val();
+  let address = $("#txtAddress").val();
+  let phone = $("#txtPhone").val();
+  let payment = $("#selPayment").val();
+  let color = $("#txtColor").val();
+
+  let user =new User(email,password,first,last,age,address,phone,payment,color);
+  //validate the user
+  if(isValid(user)){
+    saveUser(user);//this fn is in the storeManager.js
+    //console.log(user);
+  }
+}
+
+function init(){
+  console.log("Init Register");
+  //hook events
+  $("#btnRegister").click(registerUser);
+
+}
+
+/*$( "li" ).hover(
+  function() {
+    $( this ).append( $( "<span> ***</span>" ) );
+  }, function() {
+    $( this ).find( "span" ).last().remove();
+  }
+);
+ 
+$( "li.fade" ).hover(function() {
+  $( this ).fadeOut( 100 );
+  $( this ).fadeIn( 500 );
+});*/
+
+window.onload=init;
---
+++ 105 Assignment 1 - Mark Omer/script/storeManager.js
@@ -0,0 +1,9 @@
+function saveUser(user){
+  console.log(user);// this is the object
+  let val=JSON.stringify(user);//parse into a JSON string
+  console.log(val);//this is the string in JSON
+  localStorage.setItem("users",val);//send it to the localStorage
+}
+function readUsers(){
+
+}
